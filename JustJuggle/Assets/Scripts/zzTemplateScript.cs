65377affc882ec1538e955bf3c8c925f22933cf0
ind9-nau <93492981+ind9-nau@users.noreply.github.com>
2023-10-24 13:47:14 -0700

    Added initial scripts

---
+++ JustJuggle/Assets/Scripts/zzTemplateScript.cs
@@ -0,0 +1,20 @@
+w
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemplateScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
