9df00a8db833b4d87685835ab2fc9520ed24d02e
ind9-nau <93492981+ind9-nau@users.noreply.github.com>
2023-12-05 09:27:14 -0700

    WebGL

--- JustJuggle/Assets/Scripts/HandleCatch.cs
+++ JustJuggle/Assets/Scripts/HandleCatch.cs
@@ -19,7 +19,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
